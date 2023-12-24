8c5d8fd10930c38abe26e9c4a0ac93d7446ed79a
jlvanhulst <jlvanhulst@gmail.com>
2023-12-24 18:46:31 -0500

    v

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='django-openai',
+    name='django-openai-assistant',
     version='0.1',
     license='MIT',
     author="Jean-Luc Vanhulst",
